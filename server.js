4b5e3f747c5d4bc25f3567b7ca7df3e3c07bfdd6
Jared Love <lovejared91@yahoo.com>
2022-09-27 06:28:11 -0500

    added port env

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -50,9 +52,10 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
-app.listen(3001, () => {
+app.listen(PORT, () => {
 
-    console.log('API server now on port 3001!');
+  console.log(`API server now on port ${PORT}!`);
+  
 });
 
 
